8a097d900b9c57857901d2991ae4f92dffb550fb
Dominykas Ivanovas <dominykas.ivanovass@gmail.com>
2021-10-12 00:51:52 +0300

    Initial code commit

---
+++ src/VismaDevTask/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VismaDevTask.Models
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Isbn { get; set; }
+    }
+}
---
+++ src/VismaDevTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VismaDevTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Visma book library :) How can I help you?");
+            Console.WriteLine("To list all the commands, enter 'help'");
+
+            var input = Console.ReadLine();
+
+            if (input.ToLower().Equals("help"))
+            {
+                Console.WriteLine("Not implemented! Closing..");
+                Environment.Exit(0);
+            }
+            else Environment.Exit(0);
+        }
+    }
+}
