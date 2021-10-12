11d5c43a20e5890743dd8e00a5456b53f2c4b93a
Dominykas Ivanovas <dominykas.ivanovass@gmail.com>
2021-10-12 18:57:31 +0300

    Add LibrarySevice

---
+++ src/VismaDevTask/ApplicationServices/LibraryServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using VismaDevTask.Interfaces;
+using VismaDevTask.Models;
+using VismaDevTask.Repositories;
+using VismaDevTask.Requests;
+
+namespace VismaDevTask.ApplicationServices
+{
+    public class LibraryServices : ILibraryServices
+    {
+        private ILibraryRepository _repository;
+
+        public LibraryServices()
+        {
+            _repository = new LibraryRepository();
+        }
+
+        public string AddBookToLibrary(BookModel book)
+        {
+            return _repository.AddBookToLibraryDatabase(book);
+        }
+
+        public IEnumerable<BookModel> ListBooksInLibrary(FilterRequest filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBookFromLibrary(BookModel book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReturnBookToLibrary(BookModel book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookModel TakeBookFromLibrary(BookModel book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/VismaDevTask/Interfaces/ILibraryServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using VismaDevTask.Models;
+using VismaDevTask.Requests;
+
+namespace VismaDevTask.Interfaces
+{
+    public interface ILibraryServices
+    {
+        string AddBookToLibrary(BookModel book);
+        BookModel TakeBookFromLibrary(BookModel book);
+        bool ReturnBookToLibrary(BookModel book);
+        IEnumerable<BookModel> ListBooksInLibrary(FilterRequest filter);
+        bool RemoveBookFromLibrary(BookModel book);
+    }
+}
