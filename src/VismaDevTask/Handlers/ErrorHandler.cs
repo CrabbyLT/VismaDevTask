341b8479babcd2589dd4e329b5abc40f73827e8e
Dominykas Ivanovas <dominykas.ivanovass@gmail.com>
2021-10-13 16:39:47 +0300

    Add error handler

---
+++ src/VismaDevTask/Handlers/ErrorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VismaDevTask.Handlers
+{
+    static class ErrorHandler
+    {
+        public static void Handle(Action func)
+        {
+            try
+            {
+                func();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Uh oh, it seems that something happened with the process. Here is the message:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
